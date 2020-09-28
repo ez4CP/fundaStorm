0dbadade24ab3731b2b66a426106a904cda26508
Darshan Patel <dhp@somaiya.edu>
2020-09-29 00:21:29 +0530

    Retrieve Ratios,Balance Sheet,Income Statement

--- data_scrape.py
+++ data_scrape.py
@@ -3,11 +3,62 @@ from urllib.request import Request, urlopen
 from collections import defaultdict
 import pandas as pd
 from selenium import webdriver
+from firebase import firebase
+
+
+####### Note:- IN CASE, CODE IS UNABLE TO RETRIEVE SECTORS AND COMPANIES, USE THE COMMENTED CODE BELOW.
+
+
+# def get_sectors():
+#     delete_elements=['  Market Capitalisation', '  Net Sales', '  Net Profit','  Total Assets','  Quarterly Growth'
+#     ,'  Excise', '  Other Income', '  Raw Materials', '  Power & Fuel', '  Employee Cost', '  PBDIT', '  Interest', '  Tax',
+#      '  EPS', '  Investments', '  Sundry Debtors', '  Cash/Bank', '  Inventory', '  Debt', '  Contingent Liabilities', 
+#      '  Price Earning Ratios', '  Cash P/E Ratios']
+
+
+#     # req = Request('https://www.moneycontrol.com/stocks/marketinfo/marketcap/nse/index.html', headers={'User-Agent': 'Mozilla/5.0'})
+#     req = Request('http://www.moneycontrol.com/stocks/sectors/aluminium.html', headers={'User-Agent': 'Mozilla/5.0'})
+#     webpage = urlopen(req).read()
+#     sc=bs.BeautifulSoup(webpage,'lxml')
+#     dict_sectors=defaultdict()
+    
+#     for item in sc.findAll("div", {"class": "budgright_list expview-scroll MT10"}):
+#         sub_items = item.findAll('li')
+#         for sub_item in sub_items:
+#             dict_sectors[sub_item.text] =  sub_item.find('a')['href']
+#     print(dict_sectors)
+#     for i in delete_elements:
+#         dict_sectors.pop(i)
+#     return dict_sectors
+
+# def companies_by_sectors(sector_link):
+#     req = Request(sector_link, headers={'User-Agent': 'Mozilla/5.0'})
+#     dict_companies=defaultdict()
+#     webpage = urlopen(req).read()
+#     sc=bs.BeautifulSoup(webpage,'lxml')
+#     flag = 0
+#     for item in sc.findAll("div", {"class": "top_gain_table MT10"}):
+#         for a in item.findAll("a"):
+#             dict_companies[a.text]='http://www.moneycontrol.com'+a['href']
+#             flag=1
+#     if flag==0:
+#         driver = webdriver.Chrome('D:/fundaStorm/fundaStorm/chromedriver_win32/chromedriver.exe')
+#         driver.get(sector_link)
+#         all_companies=driver.find_elements_by_xpath('//*[@id="topcomp"]/div/div')
+#         for company in all_companies:
+#             elems = company.find_elements_by_tag_name('a')
+#             for elem in elems:
+#                 text=elem.get_attribute('text')
+#                 href = elem.get_attribute('href')
+#                 dict_companies[text]=href
+    
+#     return dict_companies
 
 
 
 
-def get_sectors(sector_name):
+
+def get_sectors():
     req = Request('https://www.moneycontrol.com/stocks/marketinfo/marketcap/nse/index.html', headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(req).read()
     sc=bs.BeautifulSoup(webpage,'lxml')
@@ -15,23 +66,27 @@ def get_sectors(sector_name):
     for item in sc.findAll("div", {"class": "lftmenu"}):
         sub_items = item.findAll('li')
         for sub_item in sub_items:
-            dict_sectors[sub_item.text] =  sub_item.find('a')['href']
-    return dict_sectors[sector_name]
+            dict_sectors[sub_item.text] =  "https://www.moneycontrol.com/"+sub_item.find('a')['href']
+    dict_sectors.pop('Top 100')
+    return dict_sectors
 
 def companies_by_sectors(sector_link):
-    req = Request("https://www.moneycontrol.com/"+sector_link, headers={'User-Agent': 'Mozilla/5.0'})
+    req = Request(sector_link, headers={'User-Agent': 'Mozilla/5.0'})
     dict_companies=defaultdict()
     webpage = urlopen(req).read()
     sc=bs.BeautifulSoup(webpage,'lxml')
     for item in sc.findAll("a", {"class": "bl_12"}):
-        dict_companies[item.text]=item['href']
+        dict_companies[item.text]="https://www.moneycontrol.com/"+item['href']
     dict_companies.pop(' Customize')
     dict_companies.pop("Slide Show")
     return dict_companies
 
-def get_income(dict_companies):
+
+
+
+def get_income(company_link):
     driver = webdriver.Chrome('D:/fundaStorm/fundaStorm/chromedriver_win32/chromedriver.exe')
-    driver.get('https://www.moneycontrol.com/india/stockpricequote/computers-software/tataconsultancyservices/TCS')
+    driver.get(company_link)
     time_period=driver.find_elements_by_class_name('cIncomeStmt')
     for time in time_period:
         count=0
@@ -42,21 +97,27 @@ def get_income(dict_companies):
             except:
                 pass
             count+=1
-    
-    tbl_class=driver.find_elements_by_class_name('clearfix.MB20')
-    driver.implicitly_wait(45)
-    for tbl in tbl_class:
-        fin_tbl=tbl.find_element_by_xpath('//*[@id="IncomeStatement"]/div[2]/div/table').get_attribute('outerHTML')
-    df  = pd.read_html(fin_tbl)  
-    print(df)
+    try:
+        tbl_class=driver.find_elements_by_class_name('clearfix.MB20')
+        driver.implicitly_wait(45)
+        for tbl in tbl_class:
+            fin_tbl=tbl.find_element_by_xpath('//*[@id="IncomeStatement"]/div[2]/div/table').get_attribute('outerHTML')
+        df  = pd.read_html(fin_tbl)  
+        for i in df:
+            i=i.to_dict()
+            return i
+
+    except:
+        print("Company has stopped functioning")
     # cp=pd.read_html(x,attrs={'class': 'mctable1 thborder frtab'})
     # print(cp[0])
     driver.close()
+    return defaultdict()
     
-def get_balance(choose , options):
+def get_balance(choose , options,company_link):
 
     driver = webdriver.Chrome('D:/fundaStorm/fundaStorm/chromedriver_win32/chromedriver.exe')
-    driver.get('https://www.moneycontrol.com/india/stockpricequote/computers-software/tataconsultancyservices/TCS')
+    driver.get(company_link)
     count=0
     while count< 10:
         try:
@@ -72,20 +133,28 @@ def get_balance(choose , options):
 
     for table in tbl_class:
         try:
-            df  = pd.read_html(table.get_attribute('outerHTML'))  
+            df  = pd.read_html(table.get_attribute('outerHTML')) 
+            for d in df:
+                print(d.to_dict())
         except:
-            print(table.get_attribute('outerHTML'))
-    print(df)
+            print("Company not found")
+    
     driver.close()
     
 
     
-if __name__ == "__main__":
-    dict_sectors=get_sectors("Airlines")
-    # dict_companies =companies_by_sectors(dict_sectors)
-
-    options={'BalanceSheet' : '//*[@id="Consolidated_finance"]/div/div[1]/ul/li[2]/a' , 
-            'CashFlows' : '//*[@id="Consolidated_finance"]/div/div[1]/ul/li[3]/a',
-            'Ratios' : '//*[@id="Consolidated_finance"]/div/div[1]/ul/li[4]/a'}
-    get_balance('BalanceSheet',options)
+# if __name__ == "__main__":
+#     options={'BalanceSheet' : '//*[@id="Consolidated_finance"]/div/div[1]/ul/li[2]/a' , 
+#             'CashFlows' : '//*[@id="Consolidated_finance"]/div/div[1]/ul/li[3]/a',
+#             'Ratios' : '//*[@id="Consolidated_finance"]/div/div[1]/ul/li[4]/a'}
+#     dict_sectors=get_sectors()
+#     print(dict_sectors)
+#     for value in dict_sectors.keys():
+#         print(value)
+#         dict_companies =companies_by_sectors(dict_sectors[value])
+#         print(dict_companies)
+    
+#         for i in dict_companies.values():
+#             print(i)
+#             get_income(i)
 
